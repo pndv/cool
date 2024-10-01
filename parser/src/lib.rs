a82e5198ec2036d2f57caa3980b179c88ed25d47
Vinay Pandey <babbupandey@protonmail.com>
2024-09-30 19:41:54 -0700

    Remove lexer.rs and add initial parser tree nodes
    Deleted lexer.rs to eliminate redundant or outdated code. Introduced initial tree node structures and expressions in parser modules to establish a foundation for the parser functionality. Updated dependencies accordingly.

---
+++ parser/src/lib.rs
@@ -0,0 +1,18 @@
+mod tree_nodes;
+mod expr;
+mod nodes;
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
